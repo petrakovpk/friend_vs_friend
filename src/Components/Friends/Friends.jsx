2f3945b35264bb5ca67af1ceb54ee7e1a678de64
petrakovpk <petrakovpk@gmail.com>
2020-03-07 16:05:30 +0300

    firest

---
+++ src/Components/Friends/Friends.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {connect} from "react-redux";
+
+
+class Friends extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+
+
+        return(
+
+            <div className="container-fluid">
+
+
+
+
+            </div>
+
+        )
+
+    }
+}
+
+
+export default Friends
